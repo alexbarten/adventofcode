0b21b2057b564ae3a801f4d91bf2885df5b9c8d0
alexbarten <mail@alexbarten.com>
2022-12-01 19:06:04 +0100

    Refactor variable names

--- day01/calorie_counting_2.py
+++ day01/calorie_counting_2.py
@@ -1,29 +1,32 @@
 def main():
     calorielist = compute_calories_per_elf('day01/day01_1.txt')
-    highest_set = highest_numbers(calorielist, 3)
+    highest_set = get_highest_numbers(calorielist, 3)
+
     print("Highest number of calories carried by three elves: ",
           sum(highest_set))
 
 
-def compute_calories_per_elf(calories_per_elf):
-    current_calories = 0
-    computed_calories = list()
-    with open(calories_per_elf, 'r') as ce:
+def compute_calories_per_elf(caloriefile):
+    one_elf_cals = 0
+    all_elf_cals = list()
+
+    with open(caloriefile, 'r') as ce:
         for entry in ce:
             entry = entry.strip()
             if entry != "":
-                current_calories += int(entry)
+                one_elf_cals += int(entry)
             else:
-                computed_calories.append(current_calories)
-                current_calories = 0
-        computed_calories.append(current_calories)
+                all_elf_cals.append(one_elf_cals)
+                one_elf_cals = 0
 
-    computed_calories.sort(reverse=True)
-    return computed_calories
+        all_elf_cals.append(one_elf_cals)
+
+    all_elf_cals.sort(reverse=True)
+    return all_elf_cals
 
 
-def highest_numbers(calorieset, number):
-    return calorieset[:number]
+def get_highest_numbers(calorielist, number):
+    return calorielist[:number]
 
 
 if __name__ == "__main__":
