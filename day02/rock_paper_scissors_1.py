ca12edb8d9397e5049e870f3b9d46c8574ebd29c
alexbarten <mail@alexbarten.com>
2022-12-02 10:01:12 +0100

    Solve Day02, puzzle 1

---
+++ day02/rock_paper_scissors_1.py
@@ -0,0 +1,23 @@
+def main():
+    print(convert_input("day02/day02_1.txt"))
+
+
+def convert_input(rpsfile):
+    with open(rpsfile, 'r') as rps:
+        scores = {"A X": [4, 4], "A Y": [1, 8], "A Z": [7, 3],
+                  "B X": [8, 1], "B Y": [5, 5], "B Z": [2, 9],
+                  "C X": [3, 7], "C Y": [9, 2], "C Z": [6, 6]}
+
+        themscore, youscore = 0, 0
+
+        for entry in rps:
+            them, you = scores.get(entry.rstrip())
+
+            themscore += them
+            youscore += you
+
+        return youscore
+
+
+if __name__ == "__main__":
+    main()
