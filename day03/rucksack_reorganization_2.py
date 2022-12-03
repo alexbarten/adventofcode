6ec7cc8e1ef75ef3595cb09f5c6e3479b43020d1
alexbarten <mail@alexbarten.com>
2022-12-03 11:51:36 +0100

    Solve Day03, puzzle 2

---
+++ day03/rucksack_reorganization_2.py
@@ -0,0 +1,37 @@
+def main():
+    print(prioritise("day03/day03_1.txt"))
+
+
+def prioritise(rucksacks):
+    with open(rucksacks, 'r') as rs:
+        group = list()
+        common_items = list()
+        items_sum = 0
+        eof = False
+        rucksack = rs.readline().rstrip()
+
+        while not eof:
+            for i in range(3):
+                group.append(set(rucksack))
+
+                if i == 2:
+                    common_items.append(set.intersection(*group))
+                    group = list()
+                    i = 0
+
+                rucksack = rs.readline().rstrip()
+                if rucksack == "":
+                    eof = True
+
+        for item in common_items:
+            itemvalue = ord("".join(item))
+            if itemvalue > 90:
+                items_sum += itemvalue - 96
+            else:
+                items_sum += itemvalue - 38
+
+        return items_sum
+
+
+if __name__ == "__main__":
+    main()
