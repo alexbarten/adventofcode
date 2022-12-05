0de3b91da941805dc3f004b3fa700f1c34d0e6f9
alexbarten <mail@alexbarten.com>
2022-12-05 08:34:05 +0100

    Set up Day05, puzzle 1

---
+++ day05/supply_stacks_0.py
@@ -0,0 +1,13 @@
+def main():
+    print(create("day05/day05_0.txt"))
+
+
+def create(stacks):
+    with open(stacks, 'r') as st:
+        s1 = s2 = s3 = list()
+        for level in st:
+            if 
+
+
+if __name__ == "__main__":
+    main()
