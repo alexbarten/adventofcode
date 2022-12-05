5ec17d9ceb2696f1b552740f9e6f8dba60e9b455
alexbarten <mail@alexbarten.com>
2022-12-05 21:10:37 +0100

    Halfway

--- day05/supply_stacks_0.py
+++ day05/supply_stacks_0.py
@@ -1,13 +1,29 @@
 def main():
-    print(create("day05/day05_0.txt"))
+    print(create('day05/day05_0.txt'))
 
 
 def create(stacks):
     with open(stacks, 'r') as st:
-        s1 = s2 = s3 = list()
+        stacklist = ['', '', '']
         for level in st:
-            if 
+            if level.strip() == '':
+                continue
+            elif level[1] == '1':
+                for i in range(3):
+                    stacklist[i] = stacklist[i][::-1].rstrip()
+            elif level[0:4] == 'move':
+                nr, orig, dest = [int(i) for i in level.split() if i.isdigit()]
+                stacklist[dest-1] += stacklist[orig-1][(nr * -1):]
+                # stacklist[orig-1] = stacklist[orig-1][:len(stacklist)-nr]
+            else:
+                stackcounter = 0
+                for crate in range(1, len(level), 4):
+                    print(level[crate] + str(crate))
+                    stacklist[stackcounter] += level[crate]
+                    stackcounter += 1
+
+        return stacklist
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
