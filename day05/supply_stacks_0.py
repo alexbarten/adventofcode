2424739a0898bde370bc61ea97575eda7dd3feae
alexbarten <mail@alexbarten.com>
2022-12-05 21:57:40 +0100

    Solve Day05, test input of puzzle 1

--- day05/supply_stacks_0.py
+++ day05/supply_stacks_0.py
@@ -9,16 +9,22 @@ def create(stacks):
             if level.strip() == '':
                 continue
             elif level[1] == '1':
-                for i in range(3):
+                for i in range(len(stacklist)):
                     stacklist[i] = stacklist[i][::-1].rstrip()
+                print(stacklist)
             elif level[0:4] == 'move':
+                # print('before: ' + str(stacklist))
                 nr, orig, dest = [int(i) for i in level.split() if i.isdigit()]
-                stacklist[dest-1] += stacklist[orig-1][(nr * -1):]
-                # stacklist[orig-1] = stacklist[orig-1][:len(stacklist)-nr]
+                stacklist[dest-1] += stacklist[orig-1][:(nr * -1 - 1):-1]
+                if nr < len(stacklist[orig-1]):
+                    # stacklist[orig-1] = stacklist[orig-1][:len(stacklist)-nr]
+                    stacklist[orig-1] = stacklist[orig-1].rstrip(stacklist[orig-1][-nr])
+                else:
+                    stacklist[orig-1] = ''
+                # print('after: ' + str(stacklist))
             else:
                 stackcounter = 0
                 for crate in range(1, len(level), 4):
-                    print(level[crate] + str(crate))
                     stacklist[stackcounter] += level[crate]
                     stackcounter += 1
 
