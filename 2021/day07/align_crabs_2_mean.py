84b794a46d7608c244c04bcbb37ffacc25a0f7eb
alexbarten <mail@alexbarten.com>
2022-12-07 20:19:25 +0100

    Add my progress of 2021

---
+++ 2021/day07/align_crabs_2_mean.py
@@ -0,0 +1,39 @@
+def main():
+    crabs = crabs_to_list("src/december07/crabs.txt")
+    mean = determine_mean(crabs)
+    steps = calculate_steps(crabs, mean)
+    print("The mean is:", mean)
+    print("The number of steps for all crabs is:", steps)
+
+
+def crabs_to_list(crabsfile):
+    crabs_file = open(crabsfile, "r")
+    crabs_line = crabs_file.readline().replace("\n", "")
+    crabs = crabs_line.split(",")
+    crabs = list(map(int, crabs))
+
+    return crabs
+
+
+def determine_mean(crabs):
+    mean = int(sum(crabs) / len(crabs))
+    # TODO: Sometimes 1 must be added, but sometimes not, even though the
+    # modulus is closer to the next number in both cases. I need to find
+    # out and improve the logic to set this correctly.
+    if sum(crabs) % len(crabs) != 0:
+        mean += 1
+
+    return mean
+
+
+def calculate_steps(crabs, mean):
+    steps = 0
+    for crab in crabs:
+        for i in range(1, (abs(crab - mean) + 1)):
+            steps += i
+
+    return steps
+
+
+if __name__ == "__main__":
+    main()
