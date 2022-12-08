73ef2f2d999d0b8d30b42aad36e1a5b936d3a0d2
alexbarten <mail@alexbarten.com>
2022-12-08 23:41:04 +0100

    Slight refactor

--- 2022/day08/treetop_tree_house_2.py
+++ 2022/day08/treetop_tree_house_2.py
@@ -12,29 +12,31 @@ with open('2022/day08/day08_1.txt', 'r') as tth:
 
     visible_area = 0
     for key, val in forestmap.items():
-        visi_l = visi_u = visi_r = visi_d = 0
         x, y = key
+        visi_l = x - 1
+        visi_r = len(forestline) - x
+        visi_u = y - 1
+        visi_d = forestlines - y
+
         for i in range(x - 1, 0, -1):
-            visi_l = x - 1
             if int(forestmap[i, y]) >= int(forestmap[x, y]):
                 visi_l = x - i
                 break
         for i in range(x + 1, len(forestline) + 1):
-            visi_r = len(forestline) - x
             if int(forestmap[i, y]) >= int(forestmap[x, y]):
                 visi_r = i - x
                 break
         for i in range(y - 1, 0, -1):
-            visi_u = y - 1
             if int(forestmap[x, i]) >= int(forestmap[x, y]):
                 visi_u = y - i
                 break
         for i in range(y + 1, forestlines + 1):
-            visi_d = forestlines - y
             if int(forestmap[x, i]) >= int(forestmap[x, y]):
                 visi_d = i - y
                 break
-        if visi_l * visi_r * visi_u * visi_d > visible_area:
-            visible_area = visi_l * visi_r * visi_u * visi_d
+
+        view = visi_l * visi_r * visi_u * visi_d
+        if view > visible_area:
+            visible_area = view
 
     print(visible_area)
