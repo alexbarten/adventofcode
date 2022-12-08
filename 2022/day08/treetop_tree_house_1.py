b1eaf746ec8aa59b071f56bebf6c339832ea725b
alexbarten <mail@alexbarten.com>
2022-12-08 21:15:29 +0100

    Solve Day08, puzzle 1

--- 2022/day08/treetop_tree_house_1.py
+++ 2022/day08/treetop_tree_house_1.py
@@ -1,8 +1,11 @@
-with open('2022/day08/day08_0.txt', 'r') as tth:
+with open('2022/day08/day08_1.txt', 'r') as tth:
     x = y = 1
+    forestlines = 0
     forestmap = dict()
     for forestline in tth:
-        for tree in forestline.rstrip():
+        forestline = forestline.rstrip()
+        forestlines += 1
+        for tree in forestline:
             forestmap[x, y] = tree
             x += 1
         x = 1; y += 1
@@ -10,20 +13,25 @@ with open('2022/day08/day08_0.txt', 'r') as tth:
     visible_trees = 0
     for key, val in forestmap.items():
         if 1 in key:
-            print(key)
             visible_trees += 1
         elif len(forestline) in key:
-            print(key)
             visible_trees += 1
         else:
+            visi_l = visi_u = visi_r = visi_d = True
             x, y = key
-            surrounding = set()
-            # print(x, y)
+            for i in range(1, x):
+                if int(forestmap[i, y]) >= int(forestmap[x, y]):
+                    visi_l = False
+            for i in range(x + 1, len(forestline) + 1):
+                if int(forestmap[i, y]) >= int(forestmap[x, y]):
+                    visi_r = False
+            for i in range(1, y):
+                if int(forestmap[x, i]) >= int(forestmap[x, y]):
+                    visi_u = False
+            for i in range(y + 1, forestlines + 1):
+                if int(forestmap[x, i]) >= int(forestmap[x, y]):
+                    visi_d = False
+            if visi_l or visi_r or visi_u or visi_d:
+                visible_trees += 1
 
-            surrounding.add(int(forestmap[x-1, y]))
-            surrounding.add(int(forestmap[x+1, y]))
-            surrounding.add(int(forestmap[x, y-1]))
-            surrounding.add(int(forestmap[x, y+1]))
-            print(forestmap[x, y], surrounding)
-            if int(forestmap[x, y]) > max(surrounding):
-                print("bigger")
+    print(visible_trees)
