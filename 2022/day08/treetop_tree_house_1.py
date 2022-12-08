9e7a6a11aeee0decf965806ad01cab03d8501edb
alexbarten <mail@alexbarten.com>
2022-12-08 20:34:09 +0100

    Some progres...

--- 2022/day08/treetop_tree_house_1.py
+++ 2022/day08/treetop_tree_house_1.py
@@ -7,4 +7,23 @@ with open('2022/day08/day08_0.txt', 'r') as tth:
             x += 1
         x = 1; y += 1
 
-    print(forestmap)
+    visible_trees = 0
+    for key, val in forestmap.items():
+        if 1 in key:
+            print(key)
+            visible_trees += 1
+        elif len(forestline) in key:
+            print(key)
+            visible_trees += 1
+        else:
+            x, y = key
+            surrounding = set()
+            # print(x, y)
+
+            surrounding.add(int(forestmap[x-1, y]))
+            surrounding.add(int(forestmap[x+1, y]))
+            surrounding.add(int(forestmap[x, y-1]))
+            surrounding.add(int(forestmap[x, y+1]))
+            print(forestmap[x, y], surrounding)
+            if int(forestmap[x, y]) > max(surrounding):
+                print("bigger")
