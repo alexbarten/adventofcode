2eaff49b17c4f88110c5b3b8c6a3e04f950dcd7f
alexbarten <mail@alexbarten.com>
2022-12-09 12:20:17 +0100

    Test set works fine, real input not...

---
+++ 2022/day09/rope_bridge_1.py
@@ -0,0 +1,46 @@
+with open('2022/day09/day09_test.txt', 'r') as rb:
+    head = [0, 0]
+    tail = [0, 0]
+    tailmoves = set()
+    EOF = False
+    while not EOF:
+        x_dist = abs(head[0] - tail[0])
+        y_dist = abs(head[1] - tail[1])
+
+        if x_dist > 1:
+            if head[0] > tail[0]:
+                tail[0] += 1
+            else:
+                tail[0] -= 1
+            if y_dist > 0:
+                if head[1] < tail[1]:
+                    tail[1] -= 1
+                else:
+                    tail[1] += 1
+        elif y_dist > 1:
+            if head[1] > tail[1]:
+                tail[1] += 1
+            else:
+                tail[1] -= 1
+            if x_dist > 0:
+                if head[0] < tail[0]:
+                    tail[0] -= 1
+                else:
+                    tail[0] += 1
+        else:
+            move = rb.readline()
+            if move:
+                match move[0]:
+                    case 'L':
+                        head[0] -= int(move[2])
+                    case 'R':
+                        head[0] += int(move[2])
+                    case 'U':
+                        head[1] -= int(move[2])
+                    case 'D':
+                        head[1] += int(move[2])
+            else:
+                EOF = True
+        tailmoves.add(tuple(tail))
+        print(head, tail, tailmoves)
+    print(len(tailmoves))
